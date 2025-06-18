d68ca342dcb0ef091f4991786b4394ed5880b7a7
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-18 23:19:22 +0000

    Refactor CustomNumberCounter to accept a number value and handleChange callback; update count state management

--- shared/components/form/CustomNumberCounter.tsx
+++ shared/components/form/CustomNumberCounter.tsx
@@ -2,17 +2,20 @@
 import { FC, useCallback, useState } from "react";
 
 type Props = {
-  value?: string | number;
+  value?: number;
+  handleChange?: (count: number) => void;
 };
 
-const CustomNumberCounter: FC<Props> = ({ value }) => {
+const CustomNumberCounter: FC<Props> = ({ value, handleChange }) => {
   const [count, setCount] = useState<string | number>(value ?? 0);
   const handleAddCount = useCallback(() => {
     setCount((prev) => +prev + 1);
-  }, []);
+    if (handleChange) handleChange(+count);
+  }, [count, handleChange]);
   const handleDecreaseCount = useCallback(() => {
     if (+count > 0) setCount((prev) => +prev - 1);
-  }, [count]);
+    if (handleChange) handleChange(+count);
+  }, [count, handleChange]);
   return (
     <div className="flex items-center justify-between min-w-24 bg-neutral-100 p-2 text-sm select-none">
       <button
