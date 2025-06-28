4067fa0b3331b30be1c6fd7c17467e51ec880243
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-28 19:18:08 +0000

    Enhance Zx9Speaker component by adding an image and adjusting SVG positioning for improved layout

--- features/home/Zx9Speaker.tsx
+++ features/home/Zx9Speaker.tsx
@@ -1,8 +1,11 @@
+import speaker from "@/shared/assets/images/home/desktop/image-speaker-zx9.png";
+import Image from "next/image";
+
 const Zx9Speaker = () => {
   return (
-    <section className="bg-brand rounded-lg relative max-h-[560px] ">
+    <section className="bg-brand rounded-lg relative h-[560px] ">
       <svg
-        className="relative -top-10 right-32"
+        className="absolute -left-32 -top-10  z-0"
         width="944"
         height="944"
         xmlns="http://www.w3.org/2000/svg"
@@ -13,6 +16,15 @@ const Zx9Speaker = () => {
           <circle cx="472" cy="472" r="471.5" />
         </g>
       </svg>
+      <div className="z-20 size-full relative flex items-end">
+        <Image
+          src={speaker}
+          alt="speaker"
+          height={500}
+          width={500}
+          className="w-72"
+        />
+      </div>
     </section>
   );
 };
