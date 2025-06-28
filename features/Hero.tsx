0273896372518b91bb17990a2a4778b1e998e3bd
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-06-28 08:46:19 +0000

    Update Hero component styling and content for improved presentation and responsiveness

--- features/Hero.tsx
+++ features/Hero.tsx
@@ -3,13 +3,17 @@ import CustomBtn from "@/shared/components/form/CustomBtn";
 
 const Hero = () => {
   return (
-    <section className="w-full h-[90vh] bg-cover bg-no-repeat text-white px-32 pt-20 bg-center bg-[url('/image-hero.jpg')]">
+    <section className="w-full h-[90vh] bg-cover bg-no-repeat text-white px-32 pt-20 bg-center  md:bg-[url('/image-hero.jpg')]">
       <hr className="border-neutral-100" />
-      <div className="flex items-center h-full">
+      <div className="flex items-center h-full max-w-[396px]">
         <div className="flex flex-col gap-4">
-          <h4>NEW PRODUCT</h4>
-          <p>XX99 Mark II Headphones</p>
-          <p className="w-80 text-xs">
+          <h4 className="uppercase tracking-[12px] text-sm text-neutral-50/50">
+            NEW PRODUCT
+          </h4>
+          <p className="uppercase text-[56px] font-bold font-manrope ">
+            XX99 Mark II Headphones
+          </p>
+          <p className=" md:max-w-80 text-sm text-neutral-50/80">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
