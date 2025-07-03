15378f7639ed70cfa3b999b396273071038c6b56
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-03 20:43:26 +0000

    Update XX99Mark1 component to reflect the correct product name and description; enhance content to highlight the classic features of the XX99 Mark I headphones while maintaining responsive layout and styling.

--- features/headphones/XX99Mark1.tsx
+++ features/headphones/XX99Mark1.tsx
@@ -3,20 +3,19 @@ import CustomBtn from "@/shared/components/form/CustomBtn";
 const XX99Mark1 = () => {
   return (
     <section className="flex items-center flex-col lg:flex-row flex-wrap-reverse gap-16 lg:gap-0 lg:justify-between w-full">
-      <div className=":bg-[url('/headphones/mobile/image-category-page-preview.jpg')] sm:h-[362px] w-full sm:bg-[url('/headphones/tablet/image-category-page-preview.jpg')] lg:bg-[url('/headphones/desktop/image-category-page-preview.jpg')]   rounded-md bg-cover bg-center lg:w-[540px] lg:h-[560px] " />
       <div className="flex flex-col items-center lg:items-start gap-4">
-        <h3 className="uppercase tracking-[15px] text-brand ">new product</h3>
         <p className="uppercase text-[40px] font-bold ">
-          XX99 Mark II
-          <br /> Headphones
+          XX99 Mark I<br />
+          Headphones
         </p>
         <p className="text-[15px] max-w-[445px] text-black/60 text-center lg:text-left ">
-          The new XX99 Mark II headphones is the pinnacle of pristine audio. It
-          redefines your premium headphone experience by reproducing the
-          balanced depth and precision of studio-quality sound.
+          As the gold standard for headphones, the classic XX99 Mark I offers
+          detailed and accurate audio reproduction for audiophiles, mixing
+          engineers, and music aficionados alike in studios and on the go.
         </p>
         <CustomBtn title="see product" />
       </div>
+      <div className=":bg-[url('/headphones/mobile/image-category-page-preview.jpg')] sm:h-[362px] w-full sm:bg-[url('/headphones/tablet/image-category-page-preview.jpg')] lg:bg-[url('/headphones/desktop/image-category-page-preview.jpg')]   rounded-md bg-cover bg-center lg:w-[540px] lg:h-[560px] " />
     </section>
   );
 };
