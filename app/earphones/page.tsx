fb8b97e28758cb7ea6e5c7e753a5b1d408d80d22
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-04 23:21:08 +0000

    Replace YX1Wireless component with ProductFeature in Earphones page; import earphoneFeatures constant for enhanced product display.

--- app/earphones/page.tsx
+++ app/earphones/page.tsx
@@ -1,17 +1,18 @@
-import { YX1Wireless } from "@/features";
 import {
   BestGear,
   Footer,
   Header,
   ProductCategoryList,
+  ProductFeature,
 } from "@/shared/components";
+import { earphoneFeatures } from "@/shared/constants/earphone";
 
 const page = () => {
   return (
     <main className="w-full flex flex-col gap-8 sm:gap-16 lg:gap-32  items-center">
       <Header className="bg-black static" title={"earphones"} />
       <section className=" container p-8 flex flex-col gap-8 sm:gap-16 lg:gap-32  w-full lg:px-16">
-        <YX1Wireless />
+        <ProductFeature products={earphoneFeatures} />
         <ProductCategoryList />
         <BestGear />
       </section>
