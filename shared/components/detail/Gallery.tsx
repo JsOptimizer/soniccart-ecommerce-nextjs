83185e866f316f0c9dcde189bcd181288785df68
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-06 17:33:31 +0000

    Refactor Gallery component layout and image classes for improved responsiveness and display; adjust structure to enhance visual presentation across devices.

--- shared/components/detail/Gallery.tsx
+++ shared/components/detail/Gallery.tsx
@@ -5,32 +5,33 @@ import CustomImage from "../CustomImage";
 type Props = {} & Pick<TProduct, "gallery">;
 
 const Gallery: FC<Props> = ({ gallery }) => {
-  console.log(gallery);
   return (
-    <div className="grid  grid-cols-1 sm:grid-flow-col sm:grid-cols-5 gap-8 justify-between ">
-      <div className="col-span-2 grid grid-rows-2 grid-flow-row">
+    <div className="grid  grid-cols-1  sm:grid-cols-5   sm:grid-rows-2 gap-8 w-full h-[592px] ">
+      <div className="col-span-2">
         <CustomImage
           alt={"first"}
           desktop={gallery.first.desktop}
           tablet={gallery.first.tablet}
           mobile={gallery.first.mobile}
-          className=" h-[174px] w-full lg:min-w-[445px] lg:min-h-[280px] object-cover"
-        />
-        <CustomImage
-          alt="second "
-          desktop={gallery.second.desktop}
-          tablet={gallery.second.tablet}
-          mobile={gallery.second.mobile}
-          className="h-[174px] w-full lg:min-h-[280px] lg:min-w-[445px] object-cover "
+          className="size-full  object-cover"
         />
       </div>
-      <div className="col-span-3 h-full">
+      <div className="row-span-2 col-span-3 flex  ">
         <CustomImage
           alt="third"
           desktop={gallery.third.desktop}
           tablet={gallery.third.tablet}
           mobile={gallery.third.mobile}
-          className="  min-h-full sm:min-w-[395px]  lg:min-w-[600px] object-center object-cover   "
+          className="object-cover size-full  "
+        />
+      </div>
+      <div className="col-span-2">
+        <CustomImage
+          alt="second "
+          desktop={gallery.second.desktop}
+          tablet={gallery.second.tablet}
+          mobile={gallery.second.mobile}
+          className="object-cover h-auto "
         />
       </div>
     </div>
