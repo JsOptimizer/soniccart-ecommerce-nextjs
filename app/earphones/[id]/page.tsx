40b1a8ad1ff4c5b67ef3b5bee72de6c290bf0e0f
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-06 15:27:43 +0000

    Integrate product detail fetching into earphones page; update component structure to display product information based on ID. Enhance layout with ProductDetail component for improved user experience.

--- app/earphones/[id]/page.tsx
+++ app/earphones/[id]/page.tsx
@@ -3,14 +3,25 @@ import {
   Footer,
   Header,
   ProductCategoryList,
+  ProductDetail,
   ProductsList,
 } from "@/shared/components";
+import products from "@/shared/data.json";
+import { TProduct } from "@/shared/types";
+import { FC } from "react";
 
-const page = () => {
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+const page: FC<Props> = async ({ params }) => {
+  const { id } = await params;
+  const product = products.find((data) => data.id === +id);
   return (
     <main className="w-full flex flex-col gap-32 items-center">
       <Header className="bg-black static" />
       <section className=" container p-8 flex flex-col gap-32 w-full lg:px-16">
+        <ProductDetail {...(product as TProduct)} />
         <ProductsList />
         <ProductCategoryList />
         <BestGear />
