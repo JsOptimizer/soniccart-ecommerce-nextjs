8ad3ff3b460de7abf68fcb7a257d5a303fa98781
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-06 14:43:54 +0000

    Update Gallery component layout and styling for improved responsiveness; adjust image classes for better display across devices.

--- shared/components/detail/Gallery.tsx
+++ shared/components/detail/Gallery.tsx
@@ -7,21 +7,21 @@ type Props = {} & Pick<TProduct, "gallery">;
 const Gallery: FC<Props> = ({ gallery }) => {
   console.log(gallery);
   return (
-    <div className="flex gap-8 w-full justify-between">
-      <div className="flex flex-col gap-4">
+    <div className="flex sm:gap-16 lg:gap-8 gap-8 w-full justify-between">
+      <div className="flex flex-col sm:gap16 lg:gap-8 lg:justify-between">
         <CustomImage
           alt={"first"}
           desktop={gallery.first.desktop}
           tablet={gallery.first.tablet}
           mobile={gallery.first.mobile}
-          className=" lg:min-h-[280px] "
+          className=" h-[174px] w-full lg:min-h-[280px] object-cover object-center "
         />
         <CustomImage
           alt="second "
           desktop={gallery.second.desktop}
           tablet={gallery.second.tablet}
           mobile={gallery.second.mobile}
-          className=" lg:min-h-[280px] "
+          className="h-[174px] w-full lg:min-h-[280px] object-cover object-center "
         />
       </div>
       <CustomImage
@@ -29,7 +29,7 @@ const Gallery: FC<Props> = ({ gallery }) => {
         desktop={gallery.third.desktop}
         tablet={gallery.third.tablet}
         mobile={gallery.third.mobile}
-        className="min-h-full min-w-[600px]  "
+        className="min-h-full sm:min-w-[395px]  lg:min-w-[600px] object-center object-cover  "
       />
     </div>
   );
