f28aa75c377eed531d5be787725bdc04e429e5e8
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-08 17:25:30 +0000

    Add zustand for state management and implement currencyFormatter utility function for consistent price formatting

--- shared/lib/utils.ts
+++ shared/lib/utils.ts
@@ -4,3 +4,13 @@ import { ClassNameValue, twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassNameValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function currencyFormatter() {
+  const intl = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    maximumSignificantDigits: 3,
+    currency: "usd",
+  });
+
+  return intl;
+}
