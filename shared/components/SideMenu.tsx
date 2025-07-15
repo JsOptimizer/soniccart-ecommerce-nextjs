f96c009281fe5bf077ddc15a5156838b24058a4e
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-15 16:49:37 +0000

    Enhance SideMenu component by adding current path highlighting for improved navigation clarity

--- shared/components/SideMenu.tsx
+++ shared/components/SideMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { FC } from "react";
 import { navigationLinks } from "../constants/nav-link";
 import Modal from "./Modal";
@@ -10,6 +10,7 @@ type Props = {
 };
 
 const SideMenu: FC<Props> = ({ open, onClose }) => {
+  const currentPath = usePathname();
   const router = useRouter();
   const handleLink = (path: string) => {
     router.push(path);
@@ -17,10 +18,15 @@ const SideMenu: FC<Props> = ({ open, onClose }) => {
   };
   return (
     <Modal open={open} onClose={onClose} className="justify-normal">
-      <nav className="h-full min-w-[40vw]  p-4   lg:hidden bg-white  ">
+      <nav className="h-full min-w-[40vw]  py-4   lg:hidden bg-white  ">
         <ul className="flex flex-col gap-4 mt-8">
           {navigationLinks.map(({ path, title }) => (
-            <li key={path} className="text-black/70">
+            <li
+              key={path}
+              className={`text-black/70 p-2 ${
+                currentPath === path && "bg-brand-100/20 text-brand"
+              }`}
+            >
               <button onClick={() => handleLink(path)}>{title}</button>
             </li>
           ))}
