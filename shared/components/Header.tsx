b11224ca62a921fbc855bfa2d9ab939e36e84ec4
jsoptimizer <jsoptimizer.pro@gmail.com>
2025-07-15 15:40:45 +0000

    Refactor Header component by removing redundant SVG markup for improved code clarity and maintainability.

--- shared/components/Header.tsx
+++ shared/components/Header.tsx
@@ -21,15 +21,14 @@ const Header: FC<Props> = ({ className, title = null }) => {
       )}
     >
       <div className="mx-auto container text-white sm:py-4 p-4 sm:px-8 lg:px-16 flex justify-between relative">
+        <div className="lg:hidden">
+          <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#FFF" fillRule="evenodd">
+              <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
+            </g>
+          </svg>
+        </div>
         <div className="flex items-center gap-16">
-          <div className="lg:hidden">
-            <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
-              <g fill="#FFF" fillRule="evenodd">
-                <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
-              </g>
-            </svg>
-          </div>
-
           <Image
             src={"/audiophile.png"}
             height={50}
